5b081acbf4924c4cef51655d4ef234f9920cca9f
beliyzy <belyaevds668@gmail.com>
2021-12-15 22:28:26 +0300

    form added

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <Title>Добавление товара</Title>
+    <AddForm />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'default'
+}
+</script>
+
+<style lang="scss">
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Source Sans Pro', sans-serif;
+  background: rgba(255, 254, 251, 0.8);
+}
+
+.container {
+  margin: 32px;
+}
+
+</style>
